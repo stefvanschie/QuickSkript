4045668b68cfe1a7455fe5cafce3c2b22e228449
stefvanschie <stefvanschiedev@gmail.com>
2019-01-21 20:15:57 +0100

    Add heal event

--- src/main/java/com/github/stefvanschie/quickskript/skript/SkriptLoader.java
+++ src/main/java/com/github/stefvanschie/quickskript/skript/SkriptLoader.java
@@ -431,6 +431,7 @@ public class SkriptLoader implements AutoCloseable {
                 .registerEvent("org.spigotmc.event.entity.EntityDismountEvent", "on dismount(?:ing)?", Platform.SPIGOT)
                 .registerEvent(EntityExplodeEvent.class, "on explo(?:(?:d(?:e|ing))|(?:sion))")
                 .registerEvent("org.spigotmc.event.entity.EntityMountEvent", "on mount(?:ing)?", Platform.SPIGOT)
+                .registerEvent(EntityRegainHealthEvent.class, "on heal(?:ing)?")
                 .registerEvent(EntityToggleGlideEvent.class, "on(?: toggle)? glid(?:e|ing)(?: state change)?")
                 .registerEvent(ExperienceOrbSpawnEvent.class, "on (?:(?:e)?xp(?:erience)?(?: orb)? spawn|spawn of(?: a(?:n)?)? (?:e)?xp(?:erience)?(?: orb)?)")
                 .registerEvent(ExplosionPrimeEvent.class, "on explosion prime")
