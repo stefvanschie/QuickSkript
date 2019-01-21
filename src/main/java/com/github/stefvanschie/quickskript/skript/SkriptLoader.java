ab987b3adfd2ac5be390e138a8a30ea60b93b14a
stefvanschie <stefvanschiedev@gmail.com>
2019-01-21 20:43:16 +0100

    Add inventory open event

--- src/main/java/com/github/stefvanschie/quickskript/skript/SkriptLoader.java
+++ src/main/java/com/github/stefvanschie/quickskript/skript/SkriptLoader.java
@@ -35,6 +35,7 @@ import org.bukkit.event.block.*;
 import org.bukkit.event.entity.*;
 import org.bukkit.event.inventory.FurnaceBurnEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkPopulateEvent;
@@ -437,6 +438,7 @@ public class SkriptLoader implements AutoCloseable {
                 .registerEvent(FoodLevelChangeEvent.class, "on (?:food|hunger) (?:level|met(?:er|re)|bar) chang(?:e|ing)")
                 .registerEvent(FurnaceBurnEvent.class, "on fuel burn(?:ing)?")
                 .registerEvent(InventoryCloseEvent.class, "on inventory clos(?:ed?|ing)")
+                .registerEvent(InventoryOpenEvent.class, "on inventory open(?:ed)?")
                 .registerEvent(PlayerBedEnterEvent.class, "on (?:(?:bed enter(?:ing)?)|(?:(?:player )?enter(?:ing)? (?:a )?bed))")
                 .registerEvent(PlayerBedLeaveEvent.class, "on (?:(?:bed leav(?:e|ing))|(?:(player )?leav(?:e|ing) (a )?bed))")
                 .registerEvent(PlayerBucketEmptyEvent.class, "on(?: player)?(?: empty(?:ing)?)?(?: a)? bucket(?: empty(?:ing)?)?")
