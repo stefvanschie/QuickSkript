d84c27ff359d90dfcf5bb060ac059170b26dabc8
stefvanschie <stefvanschiedev@gmail.com>
2019-01-21 20:59:13 +0100

    Add join event

--- src/main/java/com/github/stefvanschie/quickskript/skript/SkriptLoader.java
+++ src/main/java/com/github/stefvanschie/quickskript/skript/SkriptLoader.java
@@ -446,6 +446,7 @@ public class SkriptLoader implements AutoCloseable {
                 .registerEvent(PlayerCommandPreprocessEvent.class, "on command")
                 .registerEvent(PlayerFishEvent.class, "on(?: player)? fish(?:ing)?")
                 .registerEvent(PlayerItemBreakEvent.class, "on(?: player)? (?:tool break(?:ing)?|break(?:ing)?(?: (?:a|the))? tool)")
+                .registerEvent(PlayerJoinEvent.class, "on(?: player)? (?:log(?:ging )?in|join(?:ing)?)")
                 .registerEvent(PlayerLoginEvent.class, "on(?: player)? connect(?:ing)?")
                 .registerEvent(PlayerSwapHandItemsEvent.class, "on swap(ping of)?(?: (?:hand|held))? items?")
                 .registerEvent(PlayerToggleFlightEvent.class, "on(?: player)? (?:flight toggl(?:e|ing)|toggl(?:e|ing) flight)")
