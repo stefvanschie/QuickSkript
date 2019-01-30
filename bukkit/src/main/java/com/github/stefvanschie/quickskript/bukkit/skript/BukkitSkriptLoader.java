0e90475882d622321b53fa1265c2cc357e00c07e
stefvanschie <stefvanschiedev@gmail.com>
2019-01-30 20:22:08 +0100

    Add portal event

--- bukkit/src/main/java/com/github/stefvanschie/quickskript/bukkit/skript/BukkitSkriptLoader.java
+++ bukkit/src/main/java/com/github/stefvanschie/quickskript/bukkit/skript/BukkitSkriptLoader.java
@@ -246,6 +246,7 @@ public class BukkitSkriptLoader extends SkriptLoader {
                 .registerEvent(PlayerLevelChangeEvent.class, "on(?: player)? level(?: change)?")
                 .registerEvent(PlayerLocaleChangeEvent.class, "on(?: player)? (?:(?:language|locale) chang(?:e|ing)|chang(?:e|ing) (?:language|locale))")
                 .registerEvent(PlayerLoginEvent.class, "on(?: player)? connect(?:ing)?")
+                .registerEvent(PlayerPortalEvent.class, "on(?: player)? portal")
                 .registerEvent(PlayerSwapHandItemsEvent.class, "on swap(ping of)?(?: (?:hand|held))? items?")
                 .registerEvent(PlayerToggleFlightEvent.class, "on(?: player)? (?:flight toggl(?:e|ing)|toggl(?:e|ing) flight)")
         );
