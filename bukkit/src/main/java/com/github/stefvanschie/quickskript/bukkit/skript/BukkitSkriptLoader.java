3fa1453590acc05faa318b84cfd4be178cd3de28
stefvanschie <stefvanschiedev@gmail.com>
2019-03-23 19:09:07 +0100

    Add untarget event

--- bukkit/src/main/java/com/github/stefvanschie/quickskript/bukkit/skript/BukkitSkriptLoader.java
+++ bukkit/src/main/java/com/github/stefvanschie/quickskript/bukkit/skript/BukkitSkriptLoader.java
@@ -231,7 +231,7 @@ public class BukkitSkriptLoader extends SkriptLoader {
                 .registerEvent(EntityRegainHealthEvent.class, "(?:on )?heal(?:ing)?")
                 .registerEvent(EntityResurrectEvent.class, "(?:on )?(?:entity )?resurrect(?:ion)?(?: attempt)?")
                 .registerEvent(EntityTameEvent.class, "(?:on )?(?:entity )?(?:tam(?:e|ing))")
-                .registerEvent(EntityTargetEvent.class, "(?:on )?(?:entity )?target")
+                .registerEvent(EntityTargetEvent.class, "(?:on )?(?:entity )?(?:un-?)?target")
                 .registerEvent(EntityToggleGlideEvent.class, "(?:on )?(?:toggle )?glid(?:e|ing)(?: state change)?")
                 .registerEvent(EntityToggleSwimEvent.class, "(?:on )?(?:entity )?(?:toggl(?:e|ing) swim|swim toggl(?:e|ing))")
                 .registerEvent(ExperienceOrbSpawnEvent.class, "(?:on )?(?:(?:e)?xp(?:erience)?(?: orb)? spawn|spawn of(?: a(?:n)?)? (?:e)?xp(?:erience)?(?: orb)?)")
