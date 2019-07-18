b78041b963aba76976cfb18afa099287f2dc4742
stefvanschie <stefvanschiedev@gmail.com>
2019-07-18 17:56:57 +0200

    Use skript patterns for feed effect
    Signed-off-by: stefvanschie <stefvanschiedev@gmail.com>

--- core/src/main/java/com/github/stefvanschie/quickskript/core/psi/effect/PsiFeedEffect.java
+++ core/src/main/java/com/github/stefvanschie/quickskript/core/psi/effect/PsiFeedEffect.java
@@ -1,14 +1,13 @@
 package com.github.stefvanschie.quickskript.core.psi.effect;
 
 import com.github.stefvanschie.quickskript.core.context.Context;
+import com.github.stefvanschie.quickskript.core.pattern.SkriptPattern;
 import com.github.stefvanschie.quickskript.core.psi.PsiElement;
 import com.github.stefvanschie.quickskript.core.psi.PsiElementFactory;
-import com.github.stefvanschie.quickskript.core.skript.SkriptLoader;
+import com.github.stefvanschie.quickskript.core.psi.util.parsing.pattern.Pattern;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.regex.Pattern;
 
 /**
  * Feeds the player by a specified amount of hunger bars.
@@ -58,41 +57,33 @@ public class PsiFeedEffect extends PsiElement<Void> {
      *
      * @since 0.1.0
      */
-    public static class Factory implements PsiElementFactory<PsiFeedEffect> {
+    public static class Factory implements PsiElementFactory {
 
         /**
          * The pattern to match {@link PsiFeedEffect}s
          */
         @NotNull
-        private final Pattern pattern =
-            Pattern.compile("feed(?: the)? (?<player>.+?)(?: by (?<amount>.+?)(?: beefs?)?)?$");
+        private final SkriptPattern pattern = SkriptPattern.parse("feed [the] %players% [by %number% [beef[s]]]");
 
         /**
-         * {@inheritDoc}
+         * Parses the {@link #pattern} and invokes this method with its types if the match succeeds
+         *
+         * @param player the player to feed
+         * @param amount the amount of hunger bars to add to the player's hunger meter, or null
+         * @param lineNumber the line number
+         * @return the effect
+         * @since 0.1.0
          */
-        @Nullable
-        @Override
-        public PsiFeedEffect tryParse(@NotNull String text, int lineNumber) {
-            var matcher = pattern.matcher(text);
-
-            if (!matcher.matches()) {
-                return null;
-            }
-
-            var skriptLoader = SkriptLoader.get();
-
-            PsiElement<?> player = skriptLoader.forceParseElement(matcher.group("player"), lineNumber);
-
-            String amountGroup = matcher.group("amount");
-            PsiElement<?> amount = amountGroup == null ? null : skriptLoader.forceParseElement(amountGroup, lineNumber);
-
+        @NotNull
+        @Contract(pure = true)
+        @Pattern("pattern")
+        public PsiFeedEffect parse(@NotNull PsiElement<?> player, @Nullable PsiElement<?> amount, int lineNumber) {
             return create(player, amount, lineNumber);
         }
 
         /**
          * Provides a default way for creating the specified object for this factory with the given parameters as
-         * constructor parameters. This should be overridden by impl, instead of the {@link #tryParse(String, int)}
-         * method.
+         * constructor parameters.
          *
          * @param player the player to feed
          * @param amount the amount of hunger bars, or null
