6e3ffedcd3b29bc346b6825183f4b2089a94d2f4
stefvanschie <stefvanschiedev@gmail.com>
2019-07-18 17:52:39 +0200

    Use skript patterns for explosion effect
    Signed-off-by: stefvanschie <stefvanschiedev@gmail.com>

--- core/src/main/java/com/github/stefvanschie/quickskript/core/psi/effect/PsiExplosionEffect.java
+++ core/src/main/java/com/github/stefvanschie/quickskript/core/psi/effect/PsiExplosionEffect.java
@@ -1,15 +1,13 @@
 package com.github.stefvanschie.quickskript.core.psi.effect;
 
 import com.github.stefvanschie.quickskript.core.context.Context;
+import com.github.stefvanschie.quickskript.core.pattern.SkriptPattern;
 import com.github.stefvanschie.quickskript.core.psi.PsiElement;
 import com.github.stefvanschie.quickskript.core.psi.PsiElementFactory;
-import com.github.stefvanschie.quickskript.core.skript.SkriptLoader;
+import com.github.stefvanschie.quickskript.core.psi.util.parsing.pattern.Pattern;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Creates a an explosion according to the specified arguments
@@ -59,38 +57,49 @@ public class PsiExplosionEffect extends PsiElement<Void> {
      *
      * @since 0.1.0
      */
-    public static class Factory implements PsiElementFactory<PsiExplosionEffect> {
+    public static class Factory implements PsiElementFactory {
 
         /**
-         * The pattern for matching psi explosion effects
+         * The pattern for matching unsafe psi explosion effects
          */
         @NotNull
-        private final Pattern pattern = Pattern.compile(
-            "(?:(?:create|make) )?(?:an? )?(?<safe>safe )?explosion (?:of|with) (?:force|strength|power) (?<force>[\\s\\S]+)"
+        private final SkriptPattern unsafePattern = SkriptPattern.parse(
+            "[(create|make)] [an] explosion (of|with) (force|strength|power) %number% [%directions% %locations%]"
         );
 
         /**
-         * {@inheritDoc}
+         * The pattern for matching safe psi explosion effects
          */
-        @Nullable
+        @NotNull
+        private final SkriptPattern safePattern = SkriptPattern.parse(
+            "[(create|make)] [a] safe explosion (of|with) (force|strength|power) %number% [%directions% %locations%]"
+        );
+
+        /**
+         * Parses the {@link #unsafePattern} and invokes this method with its types if the match succeeds
+         */
+        @NotNull
         @Contract(pure = true)
-        @Override
-        public PsiExplosionEffect tryParse(@NotNull String text, int lineNumber) {
-            Matcher matcher = pattern.matcher(text);
+        @Pattern("unsafePattern")
+        public PsiExplosionEffect parseUnsafe(@NotNull PsiElement<?> force, @NotNull PsiElement<?> directions,
+                                        @NotNull PsiElement<?> locations, int lineNumber) {
+            return create(force, false, lineNumber);
+        }
 
-            if (!matcher.matches()) {
-                return null;
-            }
-
-            PsiElement<?> force = SkriptLoader.get().forceParseElement(matcher.group("force"), lineNumber);
-
-            return create(force, matcher.group("safe") != null, lineNumber);
+        /**
+         * Parses the {@link #safePattern} and invokes this method with its types if the match succeeds
+         */
+        @NotNull
+        @Contract(pure = true)
+        @Pattern("safePattern")
+        public PsiExplosionEffect parseSafe(@NotNull PsiElement<?> force, @NotNull PsiElement<?> directions,
+                                        @NotNull PsiElement<?> locations, int lineNumber) {
+            return create(force, true, lineNumber);
         }
 
         /**
          * Provides a default way for creating the specified object for this factory with the given parameters as
-         * constructor parameters. This should be overridden by impl, instead of the {@link #tryParse(String, int)}
-         * method.
+         * constructor parameters.
          *
          * @param force the amount of force behind this explosion
          * @param safe whether this explosion will damage the world/entities
