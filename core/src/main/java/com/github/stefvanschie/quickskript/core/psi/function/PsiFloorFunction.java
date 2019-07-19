03c6dd51281bfbcaf8d3677f8ecddcec7400f61c
stefvanschie <stefvanschiedev@gmail.com>
2019-07-19 21:28:10 +0200

    Switch to new parsing for floor function
    Signed-off-by: stefvanschie <stefvanschiedev@gmail.com>

--- core/src/main/java/com/github/stefvanschie/quickskript/core/psi/function/PsiFloorFunction.java
+++ core/src/main/java/com/github/stefvanschie/quickskript/core/psi/function/PsiFloorFunction.java
@@ -3,6 +3,7 @@ package com.github.stefvanschie.quickskript.core.psi.function;
 import com.github.stefvanschie.quickskript.core.context.Context;
 import com.github.stefvanschie.quickskript.core.psi.PsiElement;
 import com.github.stefvanschie.quickskript.core.psi.PsiElementFactory;
+import com.github.stefvanschie.quickskript.core.psi.util.parsing.Fallback;
 import com.github.stefvanschie.quickskript.core.skript.SkriptLoader;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -55,7 +56,7 @@ public class PsiFloorFunction extends PsiElement<Double> {
      *
      * @since 0.1.0
      */
-    public static class Factory implements PsiElementFactory<PsiFloorFunction> {
+    public static class Factory implements PsiElementFactory {
 
         /**
          * The pattern for matching floor expressions
@@ -64,11 +65,16 @@ public class PsiFloorFunction extends PsiElement<Double> {
         private final Pattern pattern = Pattern.compile("floor\\((?<parameter>[\\s\\S]+)\\)");
 
         /**
-         * {@inheritDoc}
+         * This gets called upon parsing
+         *
+         * @param text the text to parse
+         * @param lineNumber the line number
+         * @return the function, or null to indicate failure
+         * @since 0.1.0
          */
         @Nullable
         @Contract(pure = true)
-        @Override
+        @Fallback
         public PsiFloorFunction tryParse(@NotNull String text, int lineNumber) {
             Matcher matcher = pattern.matcher(text);
 
