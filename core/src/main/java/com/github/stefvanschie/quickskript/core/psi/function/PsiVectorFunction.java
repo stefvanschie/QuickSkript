129a0858d882786c6cda232e9b898e3a53bbd288
stefvanschie <stefvanschiedev@gmail.com>
2019-07-20 15:50:09 +0200

    Switch to new parsing for vector function
    Signed-off-by: stefvanschie <stefvanschiedev@gmail.com>

--- core/src/main/java/com/github/stefvanschie/quickskript/core/psi/function/PsiVectorFunction.java
+++ core/src/main/java/com/github/stefvanschie/quickskript/core/psi/function/PsiVectorFunction.java
@@ -3,6 +3,7 @@ package com.github.stefvanschie.quickskript.core.psi.function;
 import com.github.stefvanschie.quickskript.core.context.Context;
 import com.github.stefvanschie.quickskript.core.psi.PsiElement;
 import com.github.stefvanschie.quickskript.core.psi.PsiElementFactory;
+import com.github.stefvanschie.quickskript.core.psi.util.parsing.Fallback;
 import com.github.stefvanschie.quickskript.core.skript.SkriptLoader;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -60,7 +61,7 @@ public class PsiVectorFunction extends PsiElement<Object> {
      *
      * @since 0.1.0
      */
-    public static class Factory implements PsiElementFactory<PsiVectorFunction> {
+    public static class Factory implements PsiElementFactory {
 
         /**
          * The pattern for matching vector expressions
@@ -69,11 +70,16 @@ public class PsiVectorFunction extends PsiElement<Object> {
         private final Pattern pattern = Pattern.compile("vector\\((?<parameters>[\\s\\S]+)\\)");
 
         /**
-         * {@inheritDoc}
+         * This gets called upon parsing
+         *
+         * @param text the text to parse
+         * @param lineNumber the line number
+         * @return the function, or null to indicate failure
+         * @since 0.1.0
          */
         @Nullable
         @Contract(pure = true)
-        @Override
+        @Fallback
         public PsiVectorFunction tryParse(@NotNull String text, int lineNumber) {
             Matcher matcher = pattern.matcher(text);
 
