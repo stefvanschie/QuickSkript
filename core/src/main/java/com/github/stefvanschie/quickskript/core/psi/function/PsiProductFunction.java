997d6bbe48e7c20e89b2fedd2b3297cbf26c0790
stefvanschie <stefvanschiedev@gmail.com>
2019-07-20 15:47:49 +0200

    Switch to new parsing for product function
    Signed-off-by: stefvanschie <stefvanschiedev@gmail.com>

--- core/src/main/java/com/github/stefvanschie/quickskript/core/psi/function/PsiProductFunction.java
+++ core/src/main/java/com/github/stefvanschie/quickskript/core/psi/function/PsiProductFunction.java
@@ -5,6 +5,7 @@ import com.github.stefvanschie.quickskript.core.context.Context;
 import com.github.stefvanschie.quickskript.core.psi.PsiElement;
 import com.github.stefvanschie.quickskript.core.psi.PsiElementFactory;
 import com.github.stefvanschie.quickskript.core.psi.exception.ExecutionException;
+import com.github.stefvanschie.quickskript.core.psi.util.parsing.Fallback;
 import com.github.stefvanschie.quickskript.core.skript.SkriptLoader;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -71,7 +72,7 @@ public class PsiProductFunction extends PsiElement<Double> {
      *
      * @since 0.1.0
      */
-    public static class Factory implements PsiElementFactory<PsiProductFunction> {
+    public static class Factory implements PsiElementFactory {
 
         /**
          * The pattern for matching product expressions
@@ -80,11 +81,16 @@ public class PsiProductFunction extends PsiElement<Double> {
         private final Pattern pattern = Pattern.compile("product\\((?<parameters>[\\s\\S]+)\\)");
 
         /**
-         * {@inheritDoc}
+         * This gets called upon parsing
+         *
+         * @param text the text to parse
+         * @param lineNumber the line number
+         * @return the function, or null to indicate failure
+         * @since 0.1.0
          */
         @Nullable
         @Contract(pure = true)
-        @Override
+        @Fallback
         public PsiProductFunction tryParse(@NotNull String text, int lineNumber) {
             Matcher matcher = pattern.matcher(text);
 
