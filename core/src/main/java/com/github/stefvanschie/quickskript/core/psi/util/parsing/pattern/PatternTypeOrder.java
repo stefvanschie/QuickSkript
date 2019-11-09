cf4e300ec9453be9b444204574bb7620c7724e23
stefvanschie <stefvanschiedev@gmail.com>
2019-11-09 15:39:22 +0100

    Update javadoc for enhanced literal type method invocation
    Signed-off-by: stefvanschie <stefvanschiedev@gmail.com>

--- core/src/main/java/com/github/stefvanschie/quickskript/core/psi/util/parsing/pattern/PatternTypeOrder.java
+++ core/src/main/java/com/github/stefvanschie/quickskript/core/psi/util/parsing/pattern/PatternTypeOrder.java
@@ -48,11 +48,12 @@ public @interface PatternTypeOrder {
      * Index 0 is specified as the first parameter which gets replaced by the type. This may not actually be the first
      * parameter of the method, however. For example, if the parameters start with a parameter indicative of the
      * {@link SkriptMatchResult}, this parameter is not counted towards this order: index 0 will be the first
-     * {@link PsiElement} which indicates a type in the pattern. It is not possible to reference index -1 or lower to
-     * override earlier parameters from before this array of type parameters. Additional parameters at the end of the
-     * parameters, such as a line number, also do not count towards this count. It is also not possible to reference
-     * these later parameters by setting an index higher than the type parameter array's size - 1. For example, if a
-     * pattern with two types is referenced by the {@link Pattern}, specifying index 2 or higher is not possible.
+     * {@link PsiElement}, or {@link String} for types that have a literal constraint, which indicates a type in the
+     * pattern. It is not possible to reference index -1 or lower to override earlier parameters from before this array
+     * of type parameters. Additional parameters at the end of the parameters, such as a line number, also do not count
+     * towards this count. It is also not possible to reference these later parameters by setting an index higher than
+     * the type parameter array's size - 1. For example, if a pattern with two types is referenced by the
+     * {@link Pattern}, specifying index 2 or higher is not possible.
      *
      * @return the order in which the types should be inserted into the array
      * @since 0.1.0
