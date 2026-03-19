46a60f2be8de458279b11a1c51b01f52d3d932cd
stefvanschie <stefvanschiedev@gmail.com>
2026-03-19 21:25:23 +0100

    Fix world function
    The name supplied would sometimes be parsed as a world instead of text.
    
    Signed-off-by: stefvanschie <stefvanschiedev@gmail.com>

--- core/src/main/java/com/github/stefvanschie/quickskript/core/psi/function/PsiWorldFunction.java
+++ core/src/main/java/com/github/stefvanschie/quickskript/core/psi/function/PsiWorldFunction.java
@@ -2,16 +2,12 @@ package com.github.stefvanschie.quickskript.core.psi.function;
 
 import com.github.stefvanschie.quickskript.core.psi.PsiElement;
 import com.github.stefvanschie.quickskript.core.psi.PsiElementFactory;
-import com.github.stefvanschie.quickskript.core.psi.util.parsing.Fallback;
-import com.github.stefvanschie.quickskript.core.skript.SkriptLoader;
+import com.github.stefvanschie.quickskript.core.psi.util.parsing.pattern.Pattern;
 import com.github.stefvanschie.quickskript.core.util.Type;
 import com.github.stefvanschie.quickskript.core.util.literal.World;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Gets a world from a given string. This will never be pre computed, since there is no guarantee that any world
@@ -47,40 +43,23 @@ public class PsiWorldFunction extends PsiElement<World> {
     public static class Factory implements PsiElementFactory {
 
         /**
-         * The pattern for matching world function expressions
-         */
-        @NotNull
-        private final Pattern pattern = Pattern.compile("world\\((?<parameter>[\\s\\S]+)\\)");
-
-        /**
          * This gets called upon parsing
          *
-         * @param skriptLoader the skript loader
-         * @param text the text to parse
+         * @param name the name of the world
          * @param lineNumber the line number
          * @return the function, or null to indicate failure
          * @since 0.1.0
          */
         @Nullable
         @Contract(pure = true)
-        @Fallback
-        public PsiWorldFunction tryParse(@NotNull SkriptLoader skriptLoader, @NotNull String text, int lineNumber) {
-            Matcher matcher = pattern.matcher(text);
-
-            if (!matcher.matches()) {
-                return null;
-            }
-
-            String expression = matcher.group("parameter");
-            PsiElement<?> element = skriptLoader.forceParseElement(expression, lineNumber);
-
-            return create(element, lineNumber);
+        @Pattern("world\\(%text%\\)")
+        public PsiWorldFunction tryParse(@NotNull PsiElement<?> name, int lineNumber) {
+            return create(name, lineNumber);
         }
 
         /**
          * Provides a default way for creating the specified object for this factory with the given parameters as
-         * constructor parameters. This should be overridden by impl, instead of the
-         * {@link #tryParse(SkriptLoader, String, int)} method.
+         * constructor parameters.
          *
          * @param element the element to compute
          * @param lineNumber the line number
